2039a7bf06da8ef518f07fd04c71de928562057c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-27 17:13:16 +0900

    update

--- ci/update-pr.sh
+++ ci/update-pr.sh
@@ -4,11 +4,12 @@ set -eu
 
 cd "$(dirname "$0")/.."
 
-git fetch origin poc-1
-git checkout poc-1
+TARGET_BRANCH=poc-3
+git fetch origin "$TARGET_BRANCH"
+git checkout "$TARGET_BRANCH"
 git config user.name ci-helper
 git config user.email ci-helper@example.com
 git commit --allow-empty -m "[ci skip]"
-git push origin poc-1
+git push origin "$TARGET_BRANCH"
 git reset HEAD~1 --hard
-git push origin poc-1 --force
+git push origin "$TARGET_BRANCH" --force
