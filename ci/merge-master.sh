cbd5cda1daa7a8080b3eccd08879db62b2544ac9
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-27 14:27:13 +0900

    ci: fix

--- ci/merge-master.sh
+++ ci/merge-master.sh
@@ -4,10 +4,10 @@ set -eux
 
 cd "$(dirname "$0")/.."
 
-git fetch origin master --prune
-
 git config user.name ci-helper
 git config user.email ci-helper@example.com
+
+git fetch origin master --prune
 
 if ! git merge origin/master; then
   exit 1
