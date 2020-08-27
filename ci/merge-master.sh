2f84405bd859a255aa11e4be62997743c4c63829
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-08-27 14:08:26 +0900

    ci: update

--- ci/merge-master.sh
+++ ci/merge-master.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 
 cd "$(dirname "$0")/.."
 
 git fetch origin master --prune
+
+git config user.name ci-helper
+git config user.email ci-helper@example.com
+
 if ! git merge origin/master; then
   exit 1
 fi
